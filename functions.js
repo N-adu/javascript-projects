7a80c596cf4aa6b555780942672ecccac63fe81c
N Adu <64503636+N-adu@users.noreply.github.com>
2021-06-11 22:01:25 +0100

    Fruit Salad function.

---
+++ functions.js
@@ -0,0 +1,12 @@
+let fruits = ['apple', 'pear', 'strawberry', 'lemon', 'kiwi'];
+
+function fruitSalad(){
+    let fruitSort = fruits.sort();
+    let fruitString = fruitSort.join(',');
+}
+
+fruitSalad();
+
+
+
+
