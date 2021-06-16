c0cf1f0d5e68b7b1f80d94b2664035699c1f6509
N Adu <64503636+N-adu@users.noreply.github.com>
2021-06-17 00:40:18 +0100

    Add files via upload
    Fruit Salad function. Sorting the elements of an array by alphabetical order.

--- functions.js
+++ functions.js
@@ -1,1 +1,12 @@
+let fruits = ['apple', 'pear', 'strawberry', 'lemon', 'kiwi'];
+
+function fruitSalad(){
+    let fruitSort = fruits.sort();
+    let fruitString = fruitSort.join(',');
+}
+
+fruitSalad();
+
+
+
 
